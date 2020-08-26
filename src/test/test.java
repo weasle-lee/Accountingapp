800bab7dae73d939759ffae41c5f66b9ef4526f2
이재혁 <@DESKTOP-98664C2>
2020-08-26 17:18:08 +0900

    조건문 활용

--- src/test/test.java
+++ src/test/test.java
@@ -3,18 +3,28 @@ package test;
 public class test {
 
 	public static void main(String[] args) {
-		
-		
-		  	double valueOfSupply = 10000.0;
+			 
+	        double valueOfSupply = Double.parseDouble(args[0]);
 	        double vatRate = 0.1;
 	        double expenseRate = 0.3;
 	        double vat = valueOfSupply * vatRate;
 	        double total = valueOfSupply + vat;
 	        double expense = valueOfSupply * expenseRate;
 	        double income = valueOfSupply - expense;
-	        double dividend1 = income * 0.5;
-	        double dividend2 = income * 0.3;
-	        double dividend3 = income * 0.2;
+	         
+	        double dividend1;
+	        double dividend2;
+	        double dividend3;
+	         
+	        if(income > 10000.0) {
+	            dividend1 = income * 0.5;
+	            dividend2 = income * 0.3;
+	            dividend3 = income * 0.2;
+	        } else {
+	            dividend1 = income * 1.0;
+	            dividend2 = income * 0;
+	            dividend3 = income * 0;
+	        }
 	 
 	        System.out.println("Value of supply : " + valueOfSupply);
 	        System.out.println("VAT : " + vat);
@@ -26,5 +36,5 @@ public class test {
 	        System.out.println("Dividend 3 : " + dividend3);
 	 
 	    }
-}
-
+	 
+	}
